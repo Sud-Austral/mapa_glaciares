e08df9df644b5e5df30cbe7510a5972278cf2acb
hectorflores329 <69374321+hectorflores329@users.noreply.github.com>
2021-08-10 23:13:42 -0600

    Commit

--- app.py
+++ app.py
@@ -68,7 +68,7 @@ def mapa():
         </div>
     """
 
-    iframe = folium.IFrame(html=html, width=250, height=180)
+    iframe = folium.IFrame(html=html, width=250, height=190)
     _popup = folium.Popup(iframe, max_width=2650)
 
     geojson = folium.GeoJson(json.dumps(salida), 
