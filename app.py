95fac33738c75e6de048402235d8497d89f00147
hectorflores329 <69374321+hectorflores329@users.noreply.github.com>
2021-08-18 15:38:08 -0600

    Commit

--- app.py
+++ app.py
@@ -81,7 +81,7 @@ def mapa():
         </div>
     """
 
-    iframe = folium.IFrame(html=html, width=270, height=290)
+    iframe = folium.IFrame(html=html, width=270, height=300)
     _popup = folium.Popup(iframe, max_width=2650)
 
 
@@ -237,7 +237,7 @@ def mapaPeriodo():
         </div>
     """
 
-    iframe = folium.IFrame(html=html, width=270, height=290)
+    iframe = folium.IFrame(html=html, width=270, height=300)
     _popup = folium.Popup(iframe, max_width=2650)
 
 
