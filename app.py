02f41fa228a28bdb9d1bca03c017912af16de89e
hectorflores329 <69374321+hectorflores329@users.noreply.github.com>
2021-08-19 13:32:00 -0600

    Commit

--- app.py
+++ app.py
@@ -33,6 +33,15 @@ def mapa():
     output_dict = [x for x in input_dict['features'] if x['properties']['idZonGlac'] == id]
 
     salida = {'type':'FeatureCollection','features':output_dict}
+
+    # JSON Glaciar divisiones
+
+    glaciarDivisiones = f"{url}/R10_Lim_Glaciares_FINAL_ClipRegion.json"
+
+    input_dict_div = json.loads(requests.get(glaciarDivisiones).content)
+    output_dict_div = [x for x in input_dict_div['features'] if x['properties']['idZonGlac'] == id]
+
+    salida_div = {'type':'FeatureCollection','features':output_dict_div}
     
     if (id != ""):
         ubicacion = [float(df["Y"][indx]), float(df["X"][indx])]
@@ -100,6 +109,12 @@ def mapa():
 
     popup = _popup
     popup.add_to(geojson)
+
+    geojson_div = folium.GeoJson(json.dumps(salida_div), 
+                    name="Glaciar (subdisivión)",
+                    tooltip = folium.GeoJsonTooltip(fields=["Nombre_GLA"],
+                    aliases = ['GLACIAR: '])
+                    ).add_to(m)
 
     folium.LayerControl().add_to(m)
 
