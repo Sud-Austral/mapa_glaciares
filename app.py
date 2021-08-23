1f3d86f895c10cd9adbdd518fdebb9c42ee730ee
hectorflores329 <69374321+hectorflores329@users.noreply.github.com>
2021-08-23 00:56:17 -0600

    Commit

--- app.py
+++ app.py
@@ -535,11 +535,11 @@ def mapaPeriodo():
     <div class='legend-title'><b>GLACIARES:</b> """ + str(dfDiv["NOM_REGION"][indxDiv]) + """</div>
     <div class='legend-scale'>
     <ul class='legend-labels'>
-        <li><span style='background:#BF9000;opacity:0.7;'></span>PÉRDIDA</li>
-        <li><span style='background:#47C0F8;opacity:0.7;'></span>GANANCIA</li>
-        <li><span style='background:#FFFFFF;opacity:0.7;'></span>SIN CAMBIO</li>
-        <li><span style='color:#b2ff00'>---</span>Límite complejo glaciar</li>
-        <li><span style='color:#edff37'>---</span>Subdivisión glaciar</li>
+        <li><span style='background:#BF9000;'></span>PÉRDIDA</li>
+        <li><span style='background:#47C0F8;'></span>GANANCIA</li>
+        <li><span style='background:#FFFFFF;'></span>SIN CAMBIO</li>
+        <li>---</li>Límite complejo glaciar
+        <li>---Subdivisión glaciar</li>
     </ul>
     </div>
     </div>
